8b287efc5a45af08a27391049411886aab918289
nicolaasuni <nicolaasuni@users.sourceforge.net>
2010-08-24 09:27:58 +0200

    5.8.016

--- tcpdf.php
+++ tcpdf.php
@@ -1,7 +1,7 @@
 <?php
 //============================================================+
 // File name   : tcpdf.php
-// Version     : 5.8.015
+// Version     : 5.8.016
 // Begin       : 2002-08-03
 // Last Update : 2010-08-24
 // Author      : Nicola Asuni - Tecnick.com S.r.l - Via Della Pace, 11 - 09044 - Quartucciu (CA) - ITALY - www.tecnick.com - info@tecnick.com
@@ -126,7 +126,7 @@
  * @copyright 2002-2010 Nicola Asuni - Tecnick.com S.r.l (www.tecnick.com) Via Della Pace, 11 - 09044 - Quartucciu (CA) - ITALY - www.tecnick.com - info@tecnick.com
  * @link http://www.tcpdf.org
  * @license http://www.gnu.org/copyleft/lesser.html LGPL
- * @version 5.8.015
+ * @version 5.8.016
  */
 
 /**
@@ -150,14 +150,14 @@ if (!class_exists('TCPDF', false)) {
 	/**
 	 * define default PDF document producer
 	 */
-	define('PDF_PRODUCER', 'TCPDF 5.8.015 (http://www.tcpdf.org)');
+	define('PDF_PRODUCER', 'TCPDF 5.8.016 (http://www.tcpdf.org)');
 
 	/**
 	* This is a PHP class for generating PDF documents without requiring external extensions.<br>
 	* TCPDF project (http://www.tcpdf.org) has been originally derived in 2002 from the Public Domain FPDF class by Olivier Plathey (http://www.fpdf.org), but now is almost entirely rewritten.<br>
 	* @name TCPDF
 	* @package com.tecnick.tcpdf
-	* @version 5.8.015
+	* @version 5.8.016
 	* @author Nicola Asuni - info@tecnick.com
 	* @link http://www.tcpdf.org
 	* @license http://www.gnu.org/copyleft/lesser.html LGPL
@@ -16256,9 +16256,9 @@ if (!class_exists('TCPDF', false)) {
 			$mh = $style['module_height'];
 			// get max dimensions
 			if ($this->rtl) {
-				$maxw = $this->x - $this->lMargin;
+				$maxw = $x - $this->lMargin;
 			} else {
-				$maxw = $this->w - $this->rMargin - $this->x;
+				$maxw = $this->w - $this->rMargin - $x;
 			}
 			$maxh = ($this->h - $this->tMargin - $this->bMargin);
 			$ratioHW = ($rows * $mh) / ($cols * $mw);
