7a48cc3bbac7a7f3510848bd21c057a314870b2d
nick <nick@nicktx2.(none)>
2011-10-20 21:52:49 +0100

    5.9.132

--- tcpdf.php
+++ tcpdf.php
@@ -1,9 +1,9 @@
 <?php
 //============================================================+
 // File name   : tcpdf.php
-// Version     : 5.9.131
+// Version     : 5.9.132
 // Begin       : 2002-08-03
-// Last Update : 2011-10-13
+// Last Update : 2011-10-20
 // Author      : Nicola Asuni - Tecnick.com S.r.l - Via Della Pace, 11 - 09044 - Quartucciu (CA) - ITALY - www.tecnick.com - info@tecnick.com
 // License     : http://www.tecnick.com/pagefiles/tcpdf/LICENSE.TXT GNU-LGPLv3 + YOU CAN'T REMOVE ANY TCPDF COPYRIGHT NOTICE OR LINK FROM THE GENERATED PDF DOCUMENTS.
 // -------------------------------------------------------------------
@@ -137,7 +137,7 @@
  * Tools to encode your unicode fonts are on fonts/utils directory.</p>
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
- * @version 5.9.131
+ * @version 5.9.132
  */
 
 // Main configuration file. Define the K_TCPDF_EXTERNAL_CONFIG constant to skip this file.
@@ -149,7 +149,7 @@ require_once(dirname(__FILE__).'/config/tcpdf_config.php');
  * TCPDF project (http://www.tcpdf.org) has been originally derived in 2002 from the Public Domain FPDF class by Olivier Plathey (http://www.fpdf.org), but now is almost entirely rewritten.<br>
  * @package com.tecnick.tcpdf
  * @brief PHP class for generating PDF documents without requiring external extensions.
- * @version 5.9.131
+ * @version 5.9.132
  * @author Nicola Asuni - info@tecnick.com
  */
 class TCPDF {
@@ -160,7 +160,7 @@ class TCPDF {
 	 * Current TCPDF version.
 	 * @private
 	 */
-	private $tcpdf_version = '5.9.131';
+	private $tcpdf_version = '5.9.132';
 
 	// Protected properties
 
@@ -13476,10 +13476,11 @@ class TCPDF {
 				$color_code = $this->webcolor[$color];
 			} else {
 				// spot color
-				$returncolor = $this->getSpotColor($name);
+				$returncolor = $this->getSpotColor($color);
 				if ($returncolor === false) {
 					$returncolor = $defcol;
 				}
+				return $returncolor;
 			}
 		} else {
 			$color_code = substr($color, 1);
