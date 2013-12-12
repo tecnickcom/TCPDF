e6a6ffa36cf372ff2e17a9dd1f356894f6f217f2
nicolaasuni <info@tecnick.com>
2013-12-12 18:31:42 +0000

    6.0.052 (2013-12-12) - Bug #871 "Datamatrix coding" was fixed.

--- tcpdf.php
+++ tcpdf.php
@@ -1,9 +1,9 @@
 <?php
 //============================================================+
 // File name   : tcpdf.php
-// Version     : 6.0.051
+// Version     : 6.0.052
 // Begin       : 2002-08-03
-// Last Update : 2013-12-02
+// Last Update : 2013-12-12
 // Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
 // License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
 // -------------------------------------------------------------------
@@ -104,7 +104,7 @@
  * Tools to encode your unicode fonts are on fonts/utils directory.</p>
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
- * @version 6.0.051
+ * @version 6.0.052
  */
 
 // TCPDF configuration
@@ -128,7 +128,7 @@ require_once(dirname(__FILE__).'/include/tcpdf_static.php');
  * TCPDF project (http://www.tcpdf.org) has been originally derived in 2002 from the Public Domain FPDF class by Olivier Plathey (http://www.fpdf.org), but now is almost entirely rewritten.<br>
  * @package com.tecnick.tcpdf
  * @brief PHP class for generating PDF documents without requiring external extensions.
- * @version 6.0.051
+ * @version 6.0.052
  * @author Nicola Asuni - info@tecnick.com
  */
 class TCPDF {
