ce07b409874a7ac0567b3401c765febef9efb413
nicolaasuni <info@tecnick.com>
2014-04-10 17:47:45 +0100

    6.0.065 (2014-04-10) - Bug #905 "RGB percentage color bug in convertHTMLColorToDec()" was fixed.

--- tcpdf.php
+++ tcpdf.php
@@ -1,7 +1,7 @@
 <?php
 //============================================================+
 // File name   : tcpdf.php
-// Version     : 6.0.064
+// Version     : 6.0.065
 // Begin       : 2002-08-03
 // Last Update : 2014-04-07
 // Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
@@ -104,7 +104,7 @@
  * Tools to encode your unicode fonts are on fonts/utils directory.</p>
  * @package com.tecnick.tcpdf
  * @author Nicola Asuni
- * @version 6.0.064
+ * @version 6.0.065
  */
 
 // TCPDF configuration
@@ -128,7 +128,7 @@ require_once(dirname(__FILE__).'/include/tcpdf_static.php');
  * TCPDF project (http://www.tcpdf.org) has been originally derived in 2002 from the Public Domain FPDF class by Olivier Plathey (http://www.fpdf.org), but now is almost entirely rewritten.<br>
  * @package com.tecnick.tcpdf
  * @brief PHP class for generating PDF documents without requiring external extensions.
- * @version 6.0.064
+ * @version 6.0.065
  * @author Nicola Asuni - info@tecnick.com
  */
 class TCPDF {
