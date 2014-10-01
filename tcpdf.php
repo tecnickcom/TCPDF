7b22ee70da2719ec78626fcafe5cdaf05ab3b7cf
xelan <xelan@users.noreply.github.com>
2014-10-01 11:39:08 +0200

    fixed typo

--- tcpdf.php
+++ tcpdf.php
@@ -11,7 +11,7 @@
 //
 // This file is part of TCPDF software library.
 //
-// TCPDF is free software: you can ioredistribute it and/or modify it
+// TCPDF is free software: you can redistribute it and/or modify it
 // under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
