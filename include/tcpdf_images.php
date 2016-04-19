69a5372c3942558c25d2a38141b8f9ee71cc2489
Bram Mittendorff <bram@nerds.company>
2016-04-19 11:46:55 +0200

    Changed getimagesize for getaddrinfo failed
    You will get errors when it is an external url, so escape it.

--- include/tcpdf_images.php
+++ include/tcpdf_images.php
@@ -168,7 +168,7 @@ class TCPDF_IMAGES {
 				$file = $tfile;
 			}
 		}
-		$a = getimagesize($file);
+		$a = @getimagesize($file);
 		if (empty($a)) {
 			//Missing or incorrect image file
 			return false;
