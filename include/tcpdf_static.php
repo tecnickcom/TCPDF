a9e3d5d0ad584e222f5d90f34cdedab3d7d7a0c9
Markus Gerdes <markus@madaxel.de>
2017-09-12 10:38:37 +0200

    Update tcpdf_static.php
    add URL to alternative array instead of returning directly

--- include/tcpdf_static.php
+++ include/tcpdf_static.php
@@ -1910,7 +1910,7 @@ class TCPDF_STATIC {
 		    && !preg_match('%^//%', $file)
 		) {
 		    $urldata = @parse_url($_SERVER['SCRIPT_URI']);
-		    return $urldata['scheme'].'://'.$urldata['host'].(($file[0] == '/') ? '' : '/').$file;
+		    $alt[] = $urldata['scheme'].'://'.$urldata['host'].(($file[0] == '/') ? '' : '/').$file;
 		}
 		//
 		$alt = array_unique($alt);
