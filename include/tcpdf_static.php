64fc19439863e1b1314487a72a74d9bfd0b55a53
nicolaasuni <info@tecnick.com>
2018-02-24 11:48:20 +0000

    Bump version

--- include/tcpdf_static.php
+++ include/tcpdf_static.php
@@ -55,7 +55,7 @@ class TCPDF_STATIC {
 	 * Current TCPDF version.
 	 * @private static
 	 */
-	private static $tcpdf_version = '6.2.16';
+	private static $tcpdf_version = '6.2.17';
 
 	/**
 	 * String alias for total number of pages.
