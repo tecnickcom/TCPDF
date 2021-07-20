bf02c1cb620533be4b7a85eebdda464cf00fd080
Simon Stücher <stchr@users.noreply.github.com>
2021-07-20 16:25:22 +0200

    Fix type hints (#399)
    * Fix type hints
    
    * More accurate type hint
    
    Co-authored-by: William Desportes <williamdes@wdes.fr>
    
    * document void return
    
    * document void return
    
    * Update include/tcpdf_fonts.php
    
    Co-authored-by: William Desportes <williamdes@wdes.fr>
    
    * Fix type hints
    
    Co-authored-by: William Desportes <williamdes@wdes.fr>
    Co-authored-by: Nicola Asuni <nicolaasuni@users.noreply.github.com>

--- tcpdf_barcodes_2d.php
+++ tcpdf_barcodes_2d.php
@@ -247,7 +247,7 @@ class TCPDF2DBarcode {
 	 * Set the barcode.
 	 * @param string $code code to print
  	 * @param string $type type of barcode: <ul><li>DATAMATRIX : Datamatrix (ISO/IEC 16022)</li><li>PDF417 : PDF417 (ISO/IEC 15438:2006)</li><li>PDF417,a,e,t,s,f,o0,o1,o2,o3,o4,o5,o6 : PDF417 with parameters: a = aspect ratio (width/height); e = error correction level (0-8); t = total number of macro segments; s = macro segment index (0-99998); f = file ID; o0 = File Name (text); o1 = Segment Count (numeric); o2 = Time Stamp (numeric); o3 = Sender (text); o4 = Addressee (text); o5 = File Size (numeric); o6 = Checksum (numeric). NOTES: Parameters t, s and f are required for a Macro Control Block, all other parametrs are optional. To use a comma character ',' on text options, replace it with the character 255: "\xff".</li><li>QRCODE : QRcode Low error correction</li><li>QRCODE,L : QRcode Low error correction</li><li>QRCODE,M : QRcode Medium error correction</li><li>QRCODE,Q : QRcode Better error correction</li><li>QRCODE,H : QR-CODE Best error correction</li><li>RAW: raw mode - comma-separad list of array rows</li><li>RAW2: raw mode - array rows are surrounded by square parenthesis.</li><li>TEST : Test matrix</li></ul>
- 	 * @return array
+ 	 * @return void
 	 */
 	public function setBarcode($code, $type) {
 		$mode = explode(',', $type);
