97d85316ed93915e3c4e0fe662c812362643690b
simonbuehler <simon@00010111.de>
2023-01-20 11:46:23 +0100

    fix fix

--- tcpdf.php
+++ tcpdf.php
@@ -9925,7 +9925,7 @@ class TCPDF {
 				}
 				$out .= ' >> >>';
 			}
-			$this->getFontBuffer((($this->pdfa_mode) ? 'pdfa' : '') .'helvetica');
+			$font = $this->getFontBuffer((($this->pdfa_mode) ? 'pdfa' : '') .'helvetica');
 			$out .= ' /DA (/F'.$font['i'].' 0 Tf 0 g)';
 			$out .= ' /Q '.(($this->rtl)?'2':'0');
 			//$out .= ' /XFA ';
